f69d6bb54e12d4ebdc2a22b31c03ed67aca94338
Daniel Bar <danielbar@gmail.com>
2022-11-23 11:26:19 +0200

    Connected API to Google Spreadsheet

---
+++ assets/js/myform.js
@@ -0,0 +1,28 @@
+let reports = [];
+    // example {id:1592304983049, title: 'Deadpool', year: 2015}
+    const addReport = (ev)=>{
+        ev.preventDefault();  //to stop the form submitting
+        let report = {
+            id: Date.now(),
+            LineNumber: document.getElementById('LineNumber').value,
+            stationNumber: document.getElementById('station').value
+            timedate: document.getElementById('timedate').value
+            email: document.getElementById('email').value
+            Company: document.getElementById('dropdown').value
+            message: document.getElementById('message').value
+        }
+        reports.push(report);
+        document.forms[0].reset(); // to clear the form for the next entries
+        //document.querySelector('form').reset();
+
+        //for display purposes only
+        console.warn('added' , {reports} );
+        let pre = document.querySelector('#msg pre');
+        pre.textContent = '\n' + JSON.stringify(reports, '\t', 2);
+
+        //saving to localStorage
+        localStorage.setItem('ReportsList', JSON.stringify(reports) );
+    }
+    document.addEventListener('DOMContentLoaded', ()=>{
+        document.getElementById('btn').addEventListener('click', addReport);
+    });
